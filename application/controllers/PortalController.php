394e549009d82dbf10cff5791c6629cf304c20eb
Marc Veldman <marc.veldman@flex.surfnet.nl>
2011-02-22 14:18:54 +0000

    Add gadget usage. Incomplete, to test SVN mailer.
    git-svn-id: https://svn.surfnet.nl/svn/coin-manage/trunk@8 8502fc01-1e5f-4b1e-910d-4f0e80f30287

--- application/controllers/PortalController.php
+++ application/controllers/PortalController.php
@@ -5,6 +5,7 @@ class PortalController extends Zend_Controller_Action
     {    
         $this->_helper->ContextSwitch->setAutoJsonSerialization(true)
                                     ->addActionContext('gadgetcount', 'json')
+                                    ->addActionContext('gadgetusage', 'json')
                                     ->initContext();
     }
     
@@ -12,6 +13,7 @@ class PortalController extends Zend_Controller_Action
     {
         
     }
+    
     public function gadgetcountAction ()
     {
         if($this->getRequest()->getParam('download', false))
@@ -19,39 +21,63 @@ class PortalController extends Zend_Controller_Action
             header("Content-disposition: attachment; filename=json.txt");
         }
 
-        $Result = array(
-                    array('name' => 'Dennis-Jan', 'address' => 'Toutenburg 55', 'city' => 'Vlissingen', 'state' => 'Zeeland'),
-                    array('name' => 'Richard', 'address' => 'Caland 35', 'city' => 'Vlissingen', 'state' => 'Zeeland'),
-                    array('name' => 'bert', 'address' => 'Toutenburg 55', 'city' => 'Vlissingen', 'state' => 'Zeeland'),
-                    array('name' => 'janus', 'address' => 'Caland 35', 'city' => 'Vlissingen', 'state' => 'Zeeland'),
-                    array('name' => 'frits', 'address' => 'Toutenburg 55', 'city' => 'Vlissingen', 'state' => 'Zeeland'),
-                    array('name' => 'klaas', 'address' => 'Caland 35', 'city' => 'Vlissingen', 'state' => 'Zeeland'));
+        $gadgetList = new Model_GadgetList();
+        $Result = $gadgetList->getCount();
 
         $this->view->ResultSet = $Result;
-        $this->view->totalRecords = 55;
-        $this->view->recordsReturned = 6;
+        $this->view->totalRecords = count($Result);
+        $this->view->recordsReturned = count($Result);
         $this->view->startIndex = 0;
-        $this->view->pageSize = 25;
-
+        
+        
+        
         $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/grid.ini', APPLICATION_ENV, true);
 
-        $config->columns = array('name' => array('sort' => true, 'edit' => true),
-                                'address' => array('sort' => true, 'edit' => true),
-                                'city' => array('sort' => true, 'edit' => true),
-                                'state' => array('sort' => true, 'edit' => true),
+        $config->initialSortField = 'type';
+        $config->pageSize = count($Result);
+        $config->columns = array('type' => array('sort' => false, 'edit' => false),
+                                 'num' => array('sort' => false, 'edit' => false),
                                 );
+
 
         $this->view->config = $config;
     }
     
     public function gadgetavailableAction()
     {
-        
     }
     
     public function gadgetusageAction()
     {
-        
+        if($this->getRequest()->getParam('download', false))
+        {
+            header("Content-disposition: attachment; filename=json.txt");
+        }
+
+        $limit = intval($this->getRequest()->getParam('results'));
+        $offset = intval($this->getRequest()->getParam('startIndex'));
+
+        $gadgetList = new Model_GadgetList();
+        $Result = $gadgetList->getUsage($limit, $offset);
+
+        $this->view->ResultSet = $Result;
+        $this->view->totalRecords = count($Result);
+        $this->view->recordsReturned = count($Result);
+        $this->view->startIndex = 0;
+
+
+
+        $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/grid.ini', APPLICATION_ENV, true);
+
+        $config->initialSortField = 'num';
+        $config->pageSize = 25;
+        $config->columns = array('num' => array('sort' => false, 'edit' => false),
+                                 'title' => array('sort' => false, 'edit' => false),
+                                 'author' => array('sort' => false, 'edit' => false),
+                                );
+
+
+        $this->view->config = $config;
     }
     
     public function invitestatusAction()
