aa6b4433e27fae63cff2306d22566ebe60f7ddf1
dennis-jan@ibuildings <dennis-jan.broerse@flex.surfnet.nl>
2011-02-28 15:00:54 +0000

    Removed jQuery setup
    git-svn-id: https://svn.surfnet.nl/svn/coin-manage/trunk@26 8502fc01-1e5f-4b1e-910d-4f0e80f30287

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -38,17 +38,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view->doctype('HTML5');
     }
 
-    protected function _initJQuery()
-    {
-        $this->bootstrap('view');
-        $view = $this->getResource('view');
-        $view->addHelperPath('ZendX/JQuery/View/Helper/', 'ZendX_JQuery_View_Helper');
-
-        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer();
-        $viewRenderer->setView($view);
-        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
-    }
-
     protected function _initViewHelpers() {
         $this->bootstrap ( 'view' );
 	$view = $this->getResource ( 'view' );
