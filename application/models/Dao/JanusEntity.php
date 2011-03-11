fb51a3da4f22be5e50169167986d280c35f72d0d
Marc Veldman <marc.veldman@flex.surfnet.nl>
2011-03-11 13:30:07 +0000

    Add Janus Models/Mapper/Dao
    git-svn-id: https://svn.surfnet.nl/svn/coin-manage/trunk@34 8502fc01-1e5f-4b1e-910d-4f0e80f30287

---
+++ application/models/Dao/JanusEntity.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Dao class for JanusEntity.
+ *
+ *
+ * @author marc
+ */
+class Model_Dao_JanusEntity extends Model_Dao_Abstract
+{
+   protected $_name = 'janus__entity';
+   protected $_schema = 'service_registry';
+   protected $_use_adapter = 'db_service_registry';
+}
---
+++ application/models/JanusEntity.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Janus Entity model
+ *
+ * @author marc
+ */
+class Model_JanusEntity extends Model_Abstract
+{
+
+    public function __construct()
+    {
+        $mapper = new Model_Mapper_JanusEntityMapper('Model_Dao_JanusEntity');
+        $this->setMapper($mapper);
+    }
+
+    /**
+     * Get a list of the available IdPs
+     *
+     * @param String  $order
+     * @param String  $dir
+     * @param Integer $limit
+     * @param Integer $offset
+     * @param Boolean $countOnly Return only the number of rows instead of the
+     *                           full dataset.
+     */
+    public function getAvailableIdps($order='eid', $dir='asc', $limit=null, $offset=0, $countOnly=false)
+    {
+       return $this->getMapper()->fetchAvailableType('saml20-idp',$order, $dir, $limit, $offset, $countOnly);
+    }
+
+    public function getAvailableSps($order='eid', $dir='asc', $limit=null, $offset=0, $countOnly=false)
+    {
+       return $this->getMapper()->fetchAvailableType('saml20-sp',$order, $dir, $limit, $offset, $countOnly);
+    }
+
+    public function getIdpAndSpCount($order='num', $dir='asc', $limit=null, $offset=0, $countOnly=false)
+    {
+        return $this->getMapper()->fetchIdpAndSpCount($order, $dir, $limit, $offset, $countOnly);
+    }
+}
