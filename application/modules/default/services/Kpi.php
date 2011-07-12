bc0151a6e3580f9634da5563b7500eeff1e047b2
Marc Veldman <marc.veldman@flex.surfnet.nl>
2011-07-12 14:29:19 +0000

    Add services VERS and KPI.
    git-svn-id: https://svn.surfnet.nl/svn/coin-manage/trunk@142 8502fc01-1e5f-4b1e-910d-4f0e80f30287

---
+++ application/modules/default/services/Kpi.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * SURFconext Manage
+ *
+ * LICENSE
+ *
+ * Copyright 2011 SURFnet bv, The Netherlands
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * @category  SURFconext Manage
+ * @package
+ * @copyright Copyright © 2010-2011 SURFnet bv, The Netherlands (http://www.surfnet.nl)
+ * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ */
+/**
+ * SURFconext Key Performance Indicators
+ */
+class Default_Service_Kpi
+{
+    /**
+     * KPI Identity Provider
+     *
+     * @var String
+     */
+    const KPI_IDP = 'idp';
+
+    /**
+     * KPI Service Provider
+     *
+     * @var String
+     */
+    const KPI_SP  = 'sp';
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get the total number of logins and the number of unique user logins
+     * for the previous month
+     *
+     * @param  Integer $timestamp Timestamp base
+     *                 for
+     *
+     * @return Array('total' => 123, 'unique' => 45)
+     */
+    public function getLogins($timestamp)
+    {
+        /**
+         * Get logins from last month.
+         */
+        $date = getdate($timestamp);
+        $searchFields = array(
+            'YEAR(loginstamp)'  => $date['year'],
+            'MONTH(loginstamp)' => $date['mon']
+        );
+        $params = Surfnet_Search_Parameters::create()
+                ->setSearchParams($searchFields);
+
+        $service = new EngineBlock_Service_LoginLog();
+        $results = $service->searchCountByType($params)
+                           ->getResults();
+        return $results[0]['num'];
+    }
+
+    public function getConnectedProviderTypes($timestamp)
+    {
+        $period = date('Y-m', $timestamp);
+        $searchFields = array(
+            "((`expiration` IS NULL) OR (LEFT(`expiration`,7) <= '{$period}'))"
+            . "AND (LEFT(`created`,7) <= '{$period}')"
+        );
+
+        $params = Surfnet_Search_Parameters::create()
+                ->setSearchParams($searchFields);
+        
+        $service = new ServiceRegistry_Service_JanusEntity();
+        $providerTypes = $service->searchCountTypes($params)
+                                 ->getResults();
+        var_dump($providerTypes);
+        return array(
+                        'idp' => $providerTypes[0]["num"],
+                        'sp'  => $providerTypes[1]["num"]
+                    );
+    }
+
+    /**
+     * Get the number of Identity Provider logins  in a given month.
+     *
+     * @param Integer $timestamp timestamp to determine the month
+     *                this KPI covers.
+     * @return <type>
+     */
+    public function getIdpLogins($timestamp)
+    {
+        return $this->_getProviderTypeLogins($timestamp, self::KPI_IDP);
+    }
+
+    /**
+     * Get the number of Service Provider logins  in a given month.
+     *
+     * @param Integer $timestamp timestamp to determine the month
+     *                this KPI covers.
+     * @return <type>
+     */
+    public function getSpLogins($timestamp)
+    {
+        return $this->_getProviderTypeLogins($timestamp, self::KPI_SP);
+    }
+
+    /**
+     * Get the number of logins for a provider type in given month.
+     * 
+     * @param Integer $timestamp timestamp to determine the month
+     *                this KPI covers.
+     * @param String $type Provider type (IdP or SP)
+     * @return <type> 
+     */
+    protected function _getProviderTypeLogins($timestamp, $type)
+    {
+        /**
+         * Get logins from last month.
+         */
+        $date = getdate($timestamp);
+        $searchFields = array(
+            'YEAR(loginstamp)'  => $date['year'],
+            'MONTH(loginstamp)' => $date['mon']
+        );
+
+        $params = Surfnet_Search_Parameters::create()
+                ->setSearchParams($searchFields);
+
+        $service = new EngineBlock_Service_LoginLog();
+        switch ($type) {
+            case self::KPI_IDP:
+                $logins = $service->searchCountByIdp($params)
+                                  ->getResults();
+                break;
+            case self::KPI_SP:
+                $logins = $service->searchCountBySp($params)
+                                  ->getResults();
+                break;
+            default:
+                throw new Exception("KPI Provider Type");
+                break;
+        }
+        return $logins[0]["num"];
+    }
+
+    /**
+     * Tabs with a team connected to it.
+     *
+     * @param Integer $timestamp timestamp to determine the month
+     *                this KPI covers.
+     * @return Integer
+     */
+    public function getTeamTabs($timestamp)
+    {
+        /**
+         * Teamtabs KPI per month
+         * The timestamps are in milliseconds.
+         */
+        $date = getdate($timestamp);
+        $searchFields = array(
+            'YEAR(FROM_UNIXTIME(ROUND(creation_timestamp/1000))) <= ' . $date['year'],
+            'MONTH(FROM_UNIXTIME(ROUND(creation_timestamp/1000))) <= ' . $date['mon']
+        );
+
+        $params = Surfnet_Search_Parameters::create()
+                ->setSearchParams($searchFields);
+
+        $service = new Portal_Service_Tab();
+        $results = $service->searchTeams($params)->getResults();
+        return $results[0]['num'];
+    }
+}
+
---
+++ application/modules/default/services/Vers.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * SURFconext Manage
+ *
+ * LICENSE
+ *
+ * Copyright 2011 SURFnet bv, The Netherlands
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *
+ * @category  SURFconext Manage
+ * @package
+ * @copyright Copyright © 2010-2011 SURFnet bv, The Netherlands (http://www.surfnet.nl)
+ * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ */
+/**
+ * SURFconext External reporting Service
+ */
+class Default_Service_Vers
+{
+    /**
+     *
+     * @var ExternalReporting_Insert_Client
+     */
+    protected $_client;
+
+    /**
+     * List of insert results
+     *
+     * @var Array
+     */
+    protected $_results;
+
+    public function __construct() {
+        $this->setClient();
+    }
+
+    public function setClient($client=null)
+    {
+        if (is_null($this->_client)) {
+            // Create SOAP client
+            $config = Zend_Registry::get('config');
+            $env = $config['vers']['env'];
+            $versConfig = $config['vers']['full'][$env];
+
+            /*
+             * The reporting client is an external, so the
+             * Location/ naming convention is not exactly the
+             * same. For this reason we need to include the client
+             * explicitly.
+             */
+            require_once('Surfnet/ExternalReporting/Insert/Client.php');
+            $this->_client = new ExternalReporting_Insert_Client(
+                $versConfig['wsdl_url'],
+                $versConfig['user_name'],
+                $versConfig['password']
+            );
+        } else {
+            $this->_client = $client;
+        }
+    }
+
+    /**
+     * Get the results.
+     *
+     * @return Array
+     */
+    public function getResults() {
+        return $this->_results;
+    }
+
+    /**
+     * Format the results into a human-readable message.
+     * 
+     * @return String
+     */
+    public function getResultMessage() {
+        $message = '';
+        foreach ($this->_results as $report => $return) {
+            $message .= "{$report} | {$return['ReturnText']}\n";
+        }
+        return $message;
+    }
+    
+    /**
+     * Clear the result list.
+     * 
+     */
+    protected function _clearResults() {
+        $this->_results = array();
+    }
+
+    /**
+     * Add a result to the list
+     *
+     * @param $key    Index
+     * @param $result
+     */
+    protected function _addResult($key, $result) {
+        $this->_results[$key] = $result;
+    }
+
+    /**
+     * Send a list of data to VERS.
+     * The data is a hash with as key the thing being measured.
+     * e.g. 'Number of users'
+     *
+     * @param Array  $data
+     * @param String $period
+     *
+     * @return boolean Succes or not
+     */
+    public function insertReport($data, $period)
+    {
+        /**
+         * Build 
+         */
+        $success = true;
+        $this->_clearResults();
+        $parametersArray = array();
+        foreach ($data as $key => $value) {
+            // add total
+            $parametersArray[$key] = array(
+                    "Value"             => $value,
+                    "Type"              => $key,
+                    "Instance"          => "",
+                    "DepartmentList"    => "AS",
+                    "Period"            => $period,
+                    "IsHidden"          => false,
+                    "IsKPI"             => false
+            );
+        }
+
+        /*
+         * Send reports to VERS
+         */
+        $insertResults = array();
+        $i = 0;
+        foreach ($parametersArray as $key => $parameters)
+        {
+            $result = $this->_client->insertReport($parameters);
+            if ($result['ReturnCode'] == -1) {
+                $result = $this->_client->updateReport($parameters);
+            } else if ($result['ReturnCode'] < 1) {
+                $success = false;
+            }
+            $this->_addResult($key,$result);
+        }
+        var_dump($this->getResults());
+        return $success;
+    }
+}
