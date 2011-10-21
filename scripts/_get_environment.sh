b7b173e22451c8d9c8df0e0808a93054da5ae72f
Boy Baukema <boy.baukema@flex.surfnet.nl>
2011-10-21 13:30:27 +0000

    Add standard include to get env.
    git-svn-id: https://svn.surfnet.nl/svn/coin-manage/trunk@231 8502fc01-1e5f-4b1e-910d-4f0e80f30287

---
+++ scripts/_get_environment.sh
@@ -0,0 +1,25 @@
+#Get system environment settings.
+if [ -r /etc/profile.d/surfconext.sh ] ; then
+    . /etc/profile.d/surfconext.sh
+fi
+
+HOST=`hostname`
+case "${HOST}" in
+  't03.dev.coin.surf.net'|'vip2-app1'|'vip2-app2' ) \
+     PHPBIN='/usr/bin'
+     SURFCONEXT_SRC='/opt/www/manage'
+     ;;
+  'ebdev.net' ) \
+     PHPBIN='/usr/bin'
+     SURFCONEXT_SRC='/mnt/hgfs/ebdev/surfconext-admin'
+     ;;
+  * ) \
+     echo "${0}: Unknown host, unable to determine SURFCONEXT_SRC and PHPBIN, please edit scripts"
+     exit 1
+     ;;
+esac
+
+#Override system-wide defaults.
+if [ -r ${BASEDIR}/../.surfconext ] ; then
+    . ${BASEDIR}/../.surfconext
+fi
