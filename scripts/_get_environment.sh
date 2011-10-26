94c278d5c8131b8a76df0ef1eee539fd31f8fa54
Boy Baukema <boy.baukema@flex.surfnet.nl>
2011-10-26 10:51:14 +0000

    Refactor SURFCONEXT_SRC to a better SURFCONEXT_BASE
    git-svn-id: https://svn.surfnet.nl/svn/coin-manage/trunk@233 8502fc01-1e5f-4b1e-910d-4f0e80f30287

--- scripts/_get_environment.sh
+++ scripts/_get_environment.sh
@@ -7,14 +7,14 @@ HOST=`hostname`
 case "${HOST}" in
   't03.dev.coin.surf.net'|'vip2-app1'|'vip2-app2' ) \
      PHPBIN='/usr/bin'
-     SURFCONEXT_SRC='/opt/www/manage'
+     SURFCONEXT_BASE='/opt/www/manage'
      ;;
   'ebdev.net' ) \
      PHPBIN='/usr/bin'
-     SURFCONEXT_SRC='/mnt/hgfs/ebdev/surfconext-admin'
+     SURFCONEXT_BASE='/mnt/hgfs/ebdev/surfconext-admin'
      ;;
   * ) \
-     echo "${0}: Unknown host, unable to determine SURFCONEXT_SRC and PHPBIN, please edit scripts"
+     echo "${0}: Unknown host, unable to determine SURFCONEXT_BASE and PHPBIN, please edit scripts"
      exit 1
      ;;
 esac
--- scripts/versexport.sh
+++ scripts/versexport.sh
@@ -6,4 +6,4 @@ if [ -r ${BASEDIR}/_get_environment.sh ] ; then
     . ${BASEDIR}/_get_environment.sh
 fi
 
-cd ${SURFCONEXT_SRC}/scripts/ && ${PHPBIN}/php versexport.php > /dev/null
+cd ${SURFCONEXT_BASE}/scripts/ && ${PHPBIN}/php versexport.php > /dev/null
