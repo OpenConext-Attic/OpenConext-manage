b092f6e290b2710f96c9eea549a58106d8d65185
Boy Baukema <boy.baukema@flex.surfnet.nl>
2011-11-14 12:35:50 +0000

    Updated the TestHelper to conform to new library structure
    git-svn-id: https://svn.surfnet.nl/svn/coin-manage/trunk@247 8502fc01-1e5f-4b1e-910d-4f0e80f30287

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -91,8 +91,8 @@ if (defined('TESTS_GENERATE_REPORT') && TESTS_GENERATE_REPORT === true &&
     PHPUnit_Util_Filter::addDirectoryToWhitelist($controllers);
 }
 
-require_once '../library/Surfnet/Application.php';
-$application = new Surfnet_Application(APPLICATION_ENV,
+require_once '../library/Manage/Application.php';
+$application = new Manage_Application(APPLICATION_ENV,
         APPLICATION_PATH . '/configs/application.ini');
 $application->bootstrap();
 
