83204ae050253fa11ed39baf4c6f8ae0c6018372
Niels van Dijk <niels.vandijk@surfnet.nl>
2012-01-03 10:44:41 +0000

    Updated script to reflect ACC and PROD environment
    git-svn-id: https://svn.surfnet.nl/svn/coin-manage/trunk@274 8502fc01-1e5f-4b1e-910d-4f0e80f30287

--- scripts/_get_environment.sh
+++ scripts/_get_environment.sh
@@ -4,14 +4,25 @@ if [ -r /etc/profile.d/surfconext.sh ] ; then
 fi
 
 HOST=`hostname`
+
 case "${HOST}" in
-  't03.dev.coin.surf.net'|'vip2-app1'|'vip2-app2' ) \
+  't03.dev.coin.surf.net' ) \
      PHPBIN='/usr/bin'
      SURFCONEXT_BASE='/opt/www/manage'
      ;;
   'ebdev.net' ) \
      PHPBIN='/usr/bin'
      SURFCONEXT_BASE='/mnt/hgfs/ebdev/surfconext-admin'
+     ;;
+  'acc41.coin.surf.net'|'acc42.coin.surf.net' ) \
+     #echo "ACC"
+     PHPBIN='/usr/bin'
+     SURFCONEXT_BASE='/opt/www/manage'
+     ;;
+  'prd41.coin.surf.net'|'prd42.coin.surf.net'|'prd43.coin.surf.net' ) \
+     #echo "PROD"
+     PHPBIN='/usr/bin'
+     SURFCONEXT_BASE='/opt/www/manage'
      ;;
   * ) \
      echo "${0}: Unknown host, unable to determine SURFCONEXT_BASE and PHPBIN, please edit scripts"
