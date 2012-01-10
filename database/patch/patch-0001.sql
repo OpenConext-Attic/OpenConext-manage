f67aab541a845cf89801763fa47bd057df914d6b
Boy Baukema <boy.baukema@flex.surfnet.nl>
2012-01-10 12:52:20 +0000

    Add report table. This table is used to store a monthly report definition. Basically just a method call with an object as a parameter.
    git-svn-id: https://svn.surfnet.nl/svn/coin-manage/trunk@279 8502fc01-1e5f-4b1e-910d-4f0e80f30287

--- database/patch/patch-0001.sql
+++ database/patch/patch-0001.sql
@@ -1,8 +1,18 @@
+-- Add table to define a report.
 -- Add table to store who gets which report mailed
-CREATE TABLE IF NOT EXISTS `report_recipients` (
+CREATE TABLE IF NOT EXISTS `report` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `class` VARCHAR(255) NOT NULL,
+  `method`  VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY `class_method` (`class`, `method`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `report_recipient` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `report` VARCHAR(50) NOT NULL,
+  `report_id` INT(11) NOT NULL,
   `email`  VARCHAR(100) NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE KEY `report_recipient` (report, email)
+  UNIQUE KEY `report_recipient` (`report_id`, `email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
