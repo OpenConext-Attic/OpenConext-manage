54957ffbde5a99abf8ae2e8923b6c74d9ddfcb8b
root <root@localhost.localdomain>
2012-06-27 10:27:55 +0000

    Update Manage to SimpleSAMLphp 1.9.0 and DbPatch 1.1.1

--- library/Manage/DbPatch/Core/Application.php
+++ library/Manage/DbPatch/Core/Application.php
@@ -9,25 +9,31 @@ class Manage_DbPatch_Core_Application extends DbPatch_Core_Application
     protected function getConfig($filename = null)
     {
         try {
-
-            
             $app = new Manage_Application(
-                            $this->_getEnvironment(), 
-                            APPLICATION_PATH . '/configs/application.ini');
+                $this->_getEnvironment(),
+                APPLICATION_PATH . '/configs/application.ini'
+            );
             $dbConfig = $app->getConfig()->resources->multidb->manage->toArray();
+
             $config = array(
                 'db' => array(
                     'adapter'   => $this->_convertPdoDriverToZendDbAdapter('mysql'),
-                    'params' => $dbConfig,
+                    'params' =>array(
+                        'host'      => isset($dbConfig['host'])    ? $dbConfig['host']    : 'localhost',
+                        'username'  => isset($dbConfig['user'])    ? $dbConfig['user']    : 'root',
+                        'password'  => isset($dbConfig['password'])? $dbConfig['password']: '',
+                        'dbname'    => isset($dbConfig['dbname'])  ? $dbConfig['dbname']  : 'manage',
+                        'charset'   => isset($dbConfig['charset']) ? $dbConfig['charset'] : 'utf8',
+                    ),
                 ),
                 'patch_directory' => realpath(__DIR__ . '/../../../../database/patch'),
                 'color' => true,
             );
+
+            return new Zend_Config($config, true);
         } catch (Exception $e) {
             die($e->getMessage()."\n");
         }
-        
-        return new Zend_Config($config);
     }
 
     private function _convertPdoDriverToZendDbAdapter($pdoDriver)
@@ -49,7 +55,9 @@ class Manage_DbPatch_Core_Application extends DbPatch_Core_Application
     {
         if (getenv('APPLICATION_ENV')) {
             $this->_applicationEnvironmentId = getenv('APPLICATION_ENV');
-            define('APPLICATION_ENV', $this->_applicationEnvironmentId);
+            if (!defined('APPLICATION_ENV')) {
+                define('APPLICATION_ENV', $this->_applicationEnvironmentId);
+            }
         } else {
             throw new Exception("Environment variable APPLICATION_ENV not set or empty\n");
         }
